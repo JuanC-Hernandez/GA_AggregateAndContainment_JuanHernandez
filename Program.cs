6b0300c678ea62e1ab1dd43af76948525f541f00
Juan_C_Hernandez <33169379+JuanC-Hernandez@users.noreply.github.com>
2024-01-29 21:55:40 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace GA_AggregateAndContainment_JuanHernandez
+{
+    internal class Program
+    {
+        // Juan Hernandez
+        // Guided Assingment
+        // 01/29/2024
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
